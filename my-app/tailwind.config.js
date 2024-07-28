91867af4f113c3950df3c7fafabbbe688a500f1c
Sandeep Kandrigi <108851203+skandrigi@users.noreply.github.com>
2024-07-27 21:50:34 -0500

    add tailwind

---
+++ my-app/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ content: ["./templates/curly.html"], content: ["./templates/home.html"], content: ["./templates/straight.html"]],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
