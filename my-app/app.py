7943f006cbc3139658abad38697f2762ed0b3623
Ayad Masud <67660166+amasud7@users.noreply.github.com>
2024-07-27 20:40:44 -0500

    changes to file retreival and can now get info from model

--- my-app/app.py
+++ my-app/app.py
@@ -1,26 +1,32 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
 from werkzeug.utils import secure_filename
 import os
-
+from wtforms.validators import InputRequired
+import model
+ 
 app = Flask(__name__)
-
-upload_folder = os.path.join('static', 'assets')
-app.config['UPLOAD_FOLDER'] = upload_folder
-
+app.config['SECRET_KEY'] = 'supersecretkey'
+app.config['UPLOAD'] = './uploads'
+ 
+class UploadFileForm(FlaskForm):
+    file = FileField("File", validators=[InputRequired()])
+    submit = SubmitField("Upload File")
+ 
 @app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
 def upload_file():
-    if request.method == 'POST':
-        if 'img-upload' not in request.files:
-            return jsonify({'error': 'No file part'})
-        file = request.files['img-upload']
-        if file.filename == '':
-            return jsonify({'error': 'No selected file'})
-        if file:
-            filename = secure_filename(file.filename)
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(file_path)
-            return jsonify({'success': True, 'file_path': file_path})
-    return render_template('home.html')
+    result = ""
+    form = UploadFileForm()
+    if form.validate_on_submit():
+        file = form.file.data
+        file.save(os.path.join('./uploads', secure_filename(file.filename))) # Then save the file
+        # results holds whether it is curly hair or straight hair
+        result = model.learn.predict(file.filename)[0]
+        return render_template('products.html', result=result)
+    return render_template('home.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8001)
+
