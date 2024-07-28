be087624b39bd2123e483956d454339893f957b1
Sandeep Kandrigi <108851203+skandrigi@users.noreply.github.com>
2024-07-27 22:45:29 -0500

    uninstall tailwind

--- my-app/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [ content: ["./templates/curly.html"], content: ["./templates/home.html"], content: ["./templates/straight.html"]],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
